d53d6b2f4b0b533d3b0aa8ad16421c27ea6b5420
amarNrddn <amarnrddn02@gmail.com>
2024-03-12 02:09:52 +0700

    update: env MONGODB_URL

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,43 +1,20 @@
-import mongoose, { ConnectOptions } from 'mongoose';
+import mongoose from 'mongoose'
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
-let cached = (global as any).mongoose || { conn: null, promise: null };
+let cachd =(global as any).mongoose || { conn: null, promise: null }
 
-export const conectToMongoose = async () => {
-   console.log('Connecting to MongoDB...');
+export const conectToMongoose = async() => {
+   if(cachd.conn) return cachd.conn;
 
-   if (cached.conn) {
-      console.log('Using cached connection to MongoDB');
-      return cached.conn;
-   }
+   if(!MONGODB_URL) throw new Error('MONGODB_URL is missing')
 
-   if (!MONGODB_URL) {
-      throw new Error('MONGODB_URL is missing');
-   }
+   cachd.promise = cachd.promise || mongoose.connect(MONGODB_URL, {
+      dbName: 'db_evently',
+      bufferCommands: false
+   })
 
-   console.log('Creating new connection to MongoDB');
-   cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      poolSize: parseInt(process.env.POOL_SIZE!),
-   } as ConnectOptions)
-      .then((res) => {
-         console.log(
-            'Connected to Distribution API Database - Initial Connection'
-         );
-      })
-      .catch((err) => {
-         console.log(
-            `Initial Distribution API Database connection error occured -`,
-            err
-         );
-      });
+   cachd.conn = await cachd.promise
 
-   cached.conn = await cached.promise;
-
-   console.log('Connected to MongoDB');
-   return cached.conn;
-};
+   return cachd.conn
+}
